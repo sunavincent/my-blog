e9e8e974612f9680d15dfc07be7c84de3083b30b
Susu <susu@macbook-air.home>
2025-02-12 12:20:07 +0100

    update app.js views

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static("public"));
 
-app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
 
 app.use(express.urlencoded({ extended: true }));
 
